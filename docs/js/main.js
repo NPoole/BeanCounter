4bd08fac0b5ff3aa60253ba04f6052de7097accc
Nick Poole <Nick.Poole@SparkFun.com>
2022-01-29 22:28:10 -0500

    Incremental Webpage Improvements

--- docs/js/main.js
+++ docs/js/main.js
@@ -12,6 +12,12 @@ $("#viewBody").scroll(function() {
         $("#bcBody").css("opacity", "1");
     }
 
+    if ($("#viewBody").scrollTop() == $("#viewBody").height()) {
+        $(".chunk2layer").fadeIn();
+    } else {
+        $(".chunk2layer").fadeOut();
+    }
+
     $("#bodyImg").css("marginTop", "-" + (offset * 10) + "px");
     $("#bodyImg").css("marginLeft", "-" + (offset * 10) + "px");
 
@@ -45,14 +51,15 @@ $("#viewBody").scroll(function() {
 });
 
 $(window).resize(function() {
-    $(".sectionHeader").each(function() {
-        var divRatio = $(this).width() / $(this).height();
-        $(this).find(".areaFit").css("font-size", (1 + (divRatio / 2)) * 2 + "vh");
-    });
-    $(".sectionBody").each(function() {
-        var divRatio = $(this).width() / $(this).height();
-        $(this).find(".areaFit").css("font-size", (1 + (divRatio / 2)) * 1.4 + "vh");
-    });
+
+    sizeAndPosition();
+
+});
+
+sizeAndPosition();
+
+function sizeAndPosition() {
+
     var screenRatio = $(window).width() / $(window).height();
     if (screenRatio > 1.33) {
         var padding = ($(window).width() - ($(window).height() * 1.33)) / 2;
@@ -64,48 +71,38 @@ $(window).resize(function() {
         $("#viewBody").css("padding-right", "0px");
         $("#bcBody").css("left", ($("#viewBody").width() * 0.23) + "px");
     }
+
+    $(".sectionHeader").each(function() {
+        var divRatio = $(this).width() / $(this).height();
+        $(this).find(".areaFit").css("font-size", (1 + (divRatio / 2)) * 2 + "vh");
+    });
+
+    $(".sectionBody").each(function() {
+        var divRatio = $(this).width() / $(this).height();
+        $(this).find(".areaFit").css("font-size", (1 + (divRatio / 2)) * 1.4 + "vh");
+    });
+
     $("#bcBody").css("height", ($("#viewBody").width() * 0.12) + "px");
     $("#bcBody").css("top", ((($("#viewBody").height() - $("#navBar").height()) * 0.45) + $("#navBar").height() - ($("#bcBody").height() / 2) + "px"));
+
+    $("#bcTape").css("top", $("#chunkThree")[0].getBoundingClientRect().top + "px");
     $("#bcTape").css("left", ($("#bcBody")[0].getBoundingClientRect().left + ($("#bcBody").width() * 0.13)) + "px");
     $("#bcTape").css("width", ($("#bcBody").width() * 0.07) + "px");
+
+    $("#bcTape2").css("top", $("#chunkThree")[0].getBoundingClientRect().top + "px");
     $("#bcTape2").css("left", ($("#bcBody")[0].getBoundingClientRect().left + "px"));
     $("#bcTape2").css("width", ($("#bcBody").width() * 0.07) + "px");
+
     $("#ydim").css("width", $("#bcBody").css("width"));
+    $("#ydim").css("top", $("#bcBody")[0].getBoundingClientRect().top - ($("#ydim").height() * 1.2) + "px");
+    $("#ydim").css("left", $("#bcBody").css("left"));
+
     $("#xdim").css("height", $("#bcBody").css("height"));
+    $("#xdim").css("left", $("#bcBody")[0].getBoundingClientRect().left - ($("#xdim").width() * 1.2) + "px");
+    $("#xdim").css("top", $("#bcBody").css("top"));
+
     $("#cr2032").css("height", $("#bcBody").css("height"));
-});
+    $("#cr2032").css("left", $("#bcBody")[0].getBoundingClientRect().left + $("#bcBody").width() - ($("#cr2032").width() * .5) + "px");
+    $("#cr2032").css("top", $("#bcBody")[0].getBoundingClientRect().top + ($("#cr2032").height() * .5) + "px");
 
-var screenRatio = $(window).width() / $(window).height();
-if (screenRatio > 1.33) {
-    var padding = ($(window).width() - ($(window).height() * 1.33)) / 2;
-    $("#viewBody").css("padding-left", padding + "px");
-    $("#viewBody").css("padding-right", padding + "px");
-    $("#bcBody").css("left", (($("#viewBody").width() * 0.23) + padding) + "px");
-} else {
-    $("#viewBody").css("padding-left", "0px");
-    $("#viewBody").css("padding-right", "0px");
-    $("#bcBody").css("left", ($("#viewBody").width() * 0.23) + "px");
 }
-
-$(".sectionHeader").each(function() {
-    var divRatio = $(this).width() / $(this).height();
-    $(this).find(".areaFit").css("font-size", (1 + (divRatio / 2)) * 2 + "vh");
-});
-
-$(".sectionBody").each(function() {
-    var divRatio = $(this).width() / $(this).height();
-    $(this).find(".areaFit").css("font-size", (1 + (divRatio / 2)) * 1.4 + "vh");
-});
-
-$("#bcBody").css("height", ($("#viewBody").width() * 0.12) + "px");
-$("#bcBody").css("top", ((($("#viewBody").height() - $("#navBar").height()) * 0.45) + $("#navBar").height() - ($("#bcBody").height() / 2) + "px"));
-$("#bcTape").css("top", $("#chunkThree")[0].getBoundingClientRect().top + "px");
-$("#bcTape").css("left", ($("#bcBody")[0].getBoundingClientRect().left + ($("#bcBody").width() * 0.13)) + "px");
-$("#bcTape").css("width", ($("#bcBody").width() * 0.07) + "px");
-$("#bcTape2").css("top", $("#chunkThree")[0].getBoundingClientRect().top + "px");
-$("#bcTape2").css("left", ($("#bcBody")[0].getBoundingClientRect().left + "px"));
-$("#bcTape2").css("width", ($("#bcBody").width() * 0.07) + "px");
-$(".chunk2layer").css("top", $("#chunkTwo")[0].getBoundingClientRect().top + "px");
-$("#ydim").css("width", $("#bcBody").css("width"));
-$("#xdim").css("height", $("#bcBody").css("height"));
-$("#cr2032").css("height", $("#bcBody").css("height"));
