d5b93f95560732043e749a5297ec9b23e947f1a1
Nick Poole <Nick.Poole@SparkFun.com>
2022-02-03 18:46:49 -0500

    Update main.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -12,7 +12,7 @@ $("#viewBody").scroll(function() {
         $("#bcBody").css("opacity", "1");
     }
 
-    if ($("#viewBody").scrollTop() > $("#viewBody").height() * 0.8 && $("#viewBody").scrollTop() < $("#viewBody").height() * 1.2) {
+    if ($("#viewBody").scrollTop() > $("#viewBody").height() * 0.5 && $("#viewBody").scrollTop() < $("#viewBody").height() * 1.5) {
         $(".chunk2layer").show();
     } else {
         $(".chunk2layer").hide();
